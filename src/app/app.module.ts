19c9c0eef80b67dd4685d8b68dc8d67e4bea560e
fnetodev <fneto.04@gmail.com>
2022-05-26 15:00:44 -0300

    Autenticação de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,9 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
+import { timeout } from 'rxjs';
+import { trigger } from '@angular/animations';
 
 
 // Componentes do projeto
@@ -70,7 +73,14 @@ import { LoginComponent } from './components/login/login.component';
      MatIconModule,
      MatListModule,
      MatCardModule,
-     BrowserAnimationsModule
+     BrowserAnimationsModule,
+     ToastrModule.forRoot({
+       timeOut:4000,
+       closeButton: true,
+      progressBar: true
+     }
+
+     )
   ],
   providers: [],
   bootstrap: [AppComponent]
