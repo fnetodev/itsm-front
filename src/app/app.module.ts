1e89a972dbdea90564898c0c44f519270eb581c9
fnetodev <fneto.04@gmail.com>
2022-05-26 11:43:03 -0300

    Componente login criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 // Componentes do projeto
@@ -41,7 +42,8 @@ import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-
     NavComponent,
     HomeComponent,
     HeaderComponent,
-    TecnicoListComponent
+    TecnicoListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
+//import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
-  constructor() { }
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'teste',
+      cpf: '123',
+      email: 'teste@gmail.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '15/10/15'
+    },
+    {
+      id: 2,
+      nome: 'teste2',
+      cpf: '123',
+      email: 'teste@gmail.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '15/10/15'
+    }
+  ]
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  
 
   ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll() {
+    
+    
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
